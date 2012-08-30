f511bbb2acbf510928d4d0254a418e9fca7fd076
Boris Klymko <boris.klymko@gmail.com>
2012-08-30 23:25:30 +0200

    Xorg with Nvidia drivers

--- packages/xorg/install.sh
+++ packages/xorg/install.sh
@@ -3,10 +3,11 @@
 [ -n "$APT_MGR" ] && [ -r "$LIB_FILE" ] && . "$LIB_FILE" || { echo >&2 'Need to be invoked by autoinstall.sh'; exit 1; }
 
 showTitle2 'Installing Xorg'
-showMessage 'Aptitude : Installing Xorg'
-$APT_MGR install xorg
-
-exit 0
 # Necessite l'activation des depots non-free et contrib
 showMessage 'Aptitude : Installing nvidia'
-$APT_MGR install nvidia-glx nvidia-settings libgl1-mesa-glx
+$APT_MGR install nvidia-glx nvidia-settings nvidia-xconfig
+nvidia-xconfig
+
+exit 0
+showMessage 'Aptitude : Installing Xorg'
+$APT_MGR install xorg
