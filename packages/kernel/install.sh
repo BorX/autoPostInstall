c38b066416c8248cf2639c2936a45896eb106e5f
Boris Klymko <boris.klymko@gmail.com>
2012-08-30 23:41:17 +0200

    Added a way in order to auto-configure a Xen Hypervisor

--- packages/kernel/install.sh
+++ packages/kernel/install.sh
@@ -11,3 +11,26 @@ $APT_MGR install linux-image-amd64 || exit $?
 showMessage 'Installing Grub'
 $APT_MGR install grub-pc
 
+exit 0
+# Another way in order to auto-configure a Xen Hypervisor
+
+showMessage 'Installing kernel'
+$APT_MGR_PROMPT install linux-image-xen-amd64 xen-hypervisor-4.1-amd64 xen-tools || exit $?
+
+showMessage 'Installing Grub'
+$APT_MGR install grub-pc
+
+showMessage 'Updating Grub'
+mv /etc/grub.d/{1,5}0_linux
+cat >> /etc/default/grub <<EOF
+
+# Disable OS prober to prevent virtual machines on logical volumes from appearing in the boot menu.
+GRUB_DISABLE_OS_PROBER=true
+
+# Xen boot parameters for all Xen boots
+#GRUB_CMDLINE_XEN=""
+# Xen boot parameters for non-recovery Xen boots (in addition to GRUB_CMDLINE_XEN)
+#GRUB_CMDLINE_XEN_DEFAULT=""
+EOF
+update-grub
+
