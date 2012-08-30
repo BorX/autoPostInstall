1045d3645a50467d1e3fb2fc3e9e054cfe066a60
Boris Klymko <boris.klymko@gmail.com>
2012-08-30 23:33:45 +0200

    Keyboard configuration

---
+++ packages/keyboard/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+[ -n "$APT_MGR" ] && [ -r "$LIB_FILE" ] && . "$LIB_FILE" || { echo >&2 'Need to be invoked by autoinstall.sh'; exit 1; }
+
+showTitle2 'Keyboard configuration'
+$APT_MGR install console-data || exit $?
+
