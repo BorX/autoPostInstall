9e8a7508bd16193b23419942dbf34008d32e1845
Boris Klymko <boris.klymko@gmail.com>
2012-08-30 23:31:58 +0200

    What a lot of things to do...

--- packages/configureEtc/install.sh
+++ packages/configureEtc/install.sh
@@ -3,14 +3,8 @@
 [ -r "$LIB_FILE" ] && . "$LIB_FILE" || { echo >&2 'Need to be invoked by autoinstall.sh'; exit 1; }
 
 showTitle2 'Coping files to /etc'
-# TODO
+# TODO vimrc, profile.d, interface, iptables-init, ...
 
 
 exit 0
-# /etc/dhcp/dhclient.conf
-showMessage 'Modification of /etc/dhcp/dhclient.conf'
-cat <<EOF >> '/etc/dhcp/dhclient.conf'
-# fixer dns opendns
-prepend domain-name-servers 208.67.222.222,208.67.220.220,192.168.1.1;
-EOF
 
