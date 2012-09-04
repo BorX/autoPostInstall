b34a97b4ae4201a0182afb084b407e549c20a242
Boris Klymko <boris.klymko@gmail.com>
2012-09-04 21:50:36 +0200

    Xorg installation

--- packages/xorg/install.sh
+++ packages/xorg/install.sh
@@ -3,11 +3,10 @@
 [ -n "$APT_MGR" ] && [ -r "$LIB_FILE" ] && . "$LIB_FILE" || { echo >&2 'Need to be invoked by autoinstall.sh'; exit 1; }
 
 showTitle2 'Installing Xorg'
+#$APT_MGR install xorg
+
 # Necessite l'activation des depots non-free et contrib
 showMessage 'Aptitude : Installing nvidia'
-$APT_MGR install nvidia-glx nvidia-settings nvidia-xconfig
+$APT_MGR install nvidia-glx nvidia-settings nvidia-xconfig || exit $?
 nvidia-xconfig
 
-exit 0
-showMessage 'Aptitude : Installing Xorg'
-$APT_MGR install xorg
