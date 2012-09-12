850a8a7958394256bd9d5c384f771f18558e2b59
Boris Klymko <boris.klymko@gmail.com>
2012-09-12 23:41:38 +0200

    No installation of virtualbox-ose and inkscape

--- packages/xfce/install.sh
+++ packages/xfce/install.sh
@@ -8,11 +8,11 @@ showMessage 'Aptitude : Installing Compiz'
 $APT_MGR install compiz compizconfig-settings-manager compiz-fusion-plugins-main || exit $?
 
 showMessage 'Aptitude : Installing Xfce4-session'
-$APT_MGR install xfce4-session || exit $?
+$APT_MGR install gdm3 xfce4-session || exit $?
 
 showMessage 'Aptitude : Installing Desktop minimal'
-$APT_MGR install cairo-dock hpijs hplip-gui hp-ppd gimp openoffice.org-writer openoffice.org-calc openoffice.org-impress openoffice.org-draw iceweasel-l10n-fr totem-mozilla deluge meld totem-plugins rhythmbox-plugins rhythmbox-plugin-cdrecorder inkscape virtualbox-ose filezilla vlc fslint gcalctool || exit $?
-
+$APT_MGR install cairo-dock hpijs hplip-gui hp-ppd gimp openoffice.org-writer openoffice.org-calc openoffice.org-impress openoffice.org-draw iceweasel-l10n-fr totem-mozilla deluge meld totem-plugins rhythmbox-plugins rhythmbox-plugin-cdrecorder filezilla vlc fslint gcalctool || exit $?
+#virtualbox-ose inkscape
 showMessage 'Aptitude : Installing Desktop minimal non-free'
 $APT_MGR install ttf-mscorefonts-installer conky || exit $?
 
