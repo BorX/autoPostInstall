8b4cb933b2da8d9cfb2cb6a18349ade0e179268d
Boris Klymko <boris.klymko@gmail.com>
2012-09-16 15:17:11 +0200

    Full review of APT sources

--- packages/sources/install.sh
+++ packages/sources/install.sh
@@ -2,19 +2,59 @@
 
 [ -n "$APT_MGR" ] && [ -r "$LIB_FILE" ] && . "$LIB_FILE" || { echo >&2 'Need to be invoked by autoinstall.sh'; exit 1; }
 
+installDebMultimediaSources() {
+	showMessage 'Configuration Deb-Multimedia sources'
+	cp -v deb-multimedia.list /etc/apt/sources.list.d/ || {
+		rcode=$?
+		echo >&2 "Can't copy to /etc/apt/sources.list.d/"
+		exit $rcode
+	}
+	chown root:root /etc/apt/sources.list.d/deb-multimedia.list
+	chmod 644       /etc/apt/sources.list.d/deb-multimedia.list
+}
+
+installIceweaselSources() {
+	showMessage 'Configuration Iceweasel sources'
+	cp -v iceweasel.list /etc/apt/sources.list.d/ || {
+		rcode=$?
+		echo >&2 "Can't copy to /etc/apt/sources.list.d/"
+		exit $rcode
+	}
+	chown root:root /etc/apt/sources.list.d/iceweasel.list
+	chmod 644       /etc/apt/sources.list.d/iceweasel.list
+}
+
+installCairoDockSources() {
+	showMessage 'Configuration Cairo-Dock sources'
+	cp -v cairo-dock.list /etc/apt/sources.list.d/ || {
+		rcode=$?
+		echo >&2 "Can't copy to /etc/apt/sources.list.d/"
+		exit $rcode
+	}
+	chown root:root /etc/apt/sources.list.d/cairo-dock.list
+	chmod 644       /etc/apt/sources.list.d/cairo-dock.list
+}
+
+
 showTitle2 'Installing sources'
 
 showMessage 'Sauvegarde des fichiers originaux'
-mv /etc/apt/sources.list{,_orig}
-mv /etc/apt/preferences{,_orig}
+mv -v /etc/apt/sources.list{,_orig}
+mv -v /etc/apt/preferences{,_orig}
 
+showMessage 'Configuration Debian sources'
 cp -v sources.list preferences /etc/apt/ || {
-  rcode=$?
-  echo >&2 "Can't copy to /etc/apt/"
-  exit $rcode
+	rcode=$?
+	echo >&2 "Can't copy to /etc/apt/"
+	exit $rcode
 }
 chown root:root /etc/apt/{sources.list,preferences}
 chmod 644 /etc/apt/{sources.list,preferences}
+
+installDebMultimediaSources
+#Inutile pour Sid:installIceweaselSources
+installCairoDockSources
+
 
 showTitle2 'Installing keys'
 
@@ -24,6 +64,9 @@ $APT_MGR update || exit $?
 showMessage 'Aptitude : Installing keys'
 $APT_MGR install debian-archive-keyring deb-multimedia-keyring || exit $?
 
-showMessage 'Aptitude : Updating'
-$APT_MGR update
+
+showTitle2 'Upgrading system'
+
+showMessage 'Aptitude : Full-Upgrade'
+$APT_MGR full-upgrade
 
--- packages/upgrade/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-[ -n "$APT_MGR" ] && [ -r "$LIB_FILE" ] && . "$LIB_FILE" || { echo >&2 'Need to be invoked by autoinstall.sh'; exit 1; }
-
-showTitle2 'Upgrading system'
-showMessage 'Aptitude : Full-Upgrade'
-$APT_MGR full-upgrade
-
