9889d38055bbfd5f582f3e7d32a004b3a9692818
Boris Klymko <boris.klymko@gmail.com>
2013-01-24 22:38:09 +0100

    Configuration of eth0 interface with DHCP

--- packages/configureEtc/install.sh
+++ packages/configureEtc/install.sh
@@ -3,8 +3,13 @@
 [ -r "$LIB_FILE" ] && . "$LIB_FILE" || { echo >&2 'Need to be invoked by autoinstall.sh'; exit 1; }
 
 showTitle2 'Coping files to /etc'
-# TODO vimrc, profile.d, interface, iptables-init, ...
+# TODO vimrc, profile.d, iptables-init, ...
 
+cat <<EOF >>/etc/network/interfaces
+# The primary network interface
+allow-hotplug eth0
+iface eth0 inet dhcp
+EOF
 
 exit 0
 
