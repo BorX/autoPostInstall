1e587bc84afbb64e7d0867fa1ea033494264a21a
Boris Klymko <boris.klymko@gmail.com>
2013-01-30 23:24:02 +0100

    HP Printers

--- packages/xfce/install.sh
+++ packages/xfce/install.sh
@@ -5,9 +5,9 @@
 showTitle2 'Installing Desktop'
 
 showMessage 'Aptitude : Installing Desktop minimal'
-$APT_MGR install cairo-dock cairo-dock-plug-ins gimp openoffice.org-writer openoffice.org-calc openoffice.org-impress openoffice.org-draw meld totem-plugins rhythmbox-plugins rhythmbox-plugin-cdrecorder filezilla vlc fslint gcalctool gnome-terminal || exit $?
+$APT_MGR install hpijs hplip-gui hp-ppd cairo-dock cairo-dock-plug-ins gimp openoffice.org-writer openoffice.org-calc openoffice.org-impress openoffice.org-draw meld totem-plugins rhythmbox-plugins rhythmbox-plugin-cdrecorder filezilla vlc fslint gcalctool gnome-terminal gedit file-roller evince || exit $?
 $APT_MGR install -t experimental iceweasel-l10n-fr || exit $?
-#totem-mozilla 
+#totem-mozilla
 #virtualbox-ose inkscape
 showMessage 'Aptitude : Installing Desktop minimal non-free'
 $APT_MGR install ttf-mscorefonts-installer || exit $?
